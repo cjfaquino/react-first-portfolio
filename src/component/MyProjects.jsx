dd3bf2e68fb24b83e6a24050fae48f799e02f438
Christian Aquino <cjfaquino@gmail.com>
2020-09-28 22:08:29 -0700

    update styling

--- src/component/MyProjects.jsx
+++ src/component/MyProjects.jsx
@@ -6,56 +6,64 @@ export default function MyProjects() {
       <div className="cards">
         <div>
           <img src="/imgs/tenshabu.png" alt="" />
-          <span className="project-link">
-            <a href="http://www.tenshabu.com" title="live">
-              <i class="fas fa-globe"></i>
-            </a>{' '}
-            <a href="https://github.com/cjfaquino/tenshabu" title="source">
-              <i class="fas fa-file-code"></i>
-            </a>
-          </span>
+          <a href="http://www.tenshabu.com">
+            <span className="project-link" title="live">
+              <i className="fas fa-globe"></i>
+            </span>
+          </a>
+          <a href="https://github.com/cjfaquino/tenshabu">
+            <span className="project-link" title="source">
+              <i className="fas fa-file-code"></i>
+            </span>
+          </a>
           <h3>Ten Shabu</h3>
           <p>Description</p>
-          <p className="subtext">Tech: react.js, nginx</p>
+          <p className="subtext">Tech: react.js, nginx, css</p>
         </div>
         <div>
-          <img src="/imgs/tenshabu.png" alt="" />
-          <span className="project-link">
-            <a href="https://cjfaquino.com">
-              <i class="fas fa-globe"></i>
-            </a>{' '}
-            <a href="https://github.com/cjfaquino/cjfaquino.github.io/tree/dev">
-              <i class="fas fa-file-code"></i>
-            </a>
-          </span>
+          <img src="/imgs/portfolio.png" alt="" />
+          <a href="https://cjfaquino.com">
+            <span className="project-link" title="live">
+              <i className="fas fa-globe"></i>
+            </span>
+          </a>
+          <a href="https://github.com/cjfaquino/cjfaquino.github.io/tree/dev">
+            <span className="project-link" title="source">
+              <i className="fas fa-file-code"></i>
+            </span>
+          </a>
           <h3>Portfolio Page</h3>
           <p>Description</p>
-          <p className="subtext">Tech: react.js</p>
+          <p className="subtext">Tech: react.js, css</p>
         </div>
         <div>
-          <img src="/imgs/tenshabu.png" alt="" />
-          <span className="project-link">
-            <a href="https://tenshabu.com">
-              <i class="fas fa-globe"></i>
-            </a>{' '}
-            <a href="https://github.com/cjfaquino/yelpcamp">
-              <i class="fas fa-file-code"></i>
-            </a>
-          </span>
+          <img src="/imgs/yelpcamp.png" alt="" />
+          <a href="https://tenshabu.com" title="live">
+            <span className="project-link">
+              <i className="fas fa-globe"></i>
+            </span>
+          </a>
+          <a href="https://github.com/cjfaquino/yelpcamp">
+            <span className="project-link" title="source">
+              <i className="fas fa-file-code"></i>
+            </span>
+          </a>
           <h3>Yelp Camp</h3>
           <p>Description</p>
-          <p className="subtext">Tech: mongoDB, node.js, express, passport</p>
+          <p className="subtext">Tech: mongoDB, node.js, express, passport, css</p>
         </div>
         <div>
-          <img src="/imgs/tenshabu.png" alt="" />
-          <span className="project-link">
-            <a href="https://tenshabu.com">
-              <i class="fas fa-globe"></i>
-            </a>{' '}
-            <a href="https://github.com/cjfaquino/forkify">
-              <i class="fas fa-file-code"></i>
-            </a>
-          </span>
+          <img src="/imgs/forkify.png" alt="" />
+          <a href="https://cjfaquino.com/forkify" title="live">
+            <span className="project-link">
+              <i className="fas fa-globe"></i>
+            </span>
+          </a>
+          <a href="https://github.com/cjfaquino/forkify">
+            <span className="project-link" title="source">
+              <i className="fas fa-file-code"></i>
+            </span>
+          </a>
           <h3>Forkify</h3>
           <p>Description</p>
           <p className="subtext">Tech: js, webpack, babel</p>
