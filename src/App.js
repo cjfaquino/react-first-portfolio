de20ef16cf8ca7b0bbf0c66543380dd35104c1bc
Christian Aquino <cjfaquino@gmail.com>
2020-10-08 09:09:43 -0700

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { useState } from 'react';
 import MyFooter from './component/MyFooter';
 import MyPortfolio from './component/MyPortfolio';
+import MyHeader from './component/MyHeader';
 import { Collapse } from 'react-collapse';
-import { Link } from 'react-scroll';
 import './App.css';
 
 export default function App() {
   const [open, setOpen] = useState(false);
   return (
     <div className="App">
-      <header>
-        <div className="jumbo container">
-          <h1>Christian Aquino</h1>
-          <h4>Web Design & Front End Development</h4>
-          <Link to={!open ? 'projects' : ''} spy={true} smooth={true} duration={500} onClick={() => setOpen(!open)}>
-            Projects
-          </Link>
-        </div>
-      </header>
-
+      <MyHeader open={!open} setOpen={setOpen} />
       <Collapse isOpened={open}>
         <MyPortfolio />
       </Collapse>
---
+++ src/component/MyHeader.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+
+export default function MyHeader(props) {
+  return (
+    <header>
+      <div className="jumbo container">
+        <h1>Christian Aquino</h1>
+        <h4>Web Design & Front End Development</h4>
+        <Link
+          to={props.open ? 'projects' : ''}
+          spy={true}
+          smooth={true}
+          delay={50}
+          duration={700}
+          onClick={() => {
+            props.setOpen(props.open);
+          }}
+        >
+          Projects
+        </Link>
+      </div>
+    </header>
+  );
+}
