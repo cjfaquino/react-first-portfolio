afa44e89ce61ab7d5d5cf5ddd3ca8cf6eeb4192f
Christian Aquino <cjfaquino@gmail.com>
2020-10-15 20:31:24 -0700

    fix element not found

--- src/component/MyHeader.jsx
+++ src/component/MyHeader.jsx
@@ -1,24 +1,45 @@
+import { faAngleDoubleDown, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-scroll';
 
 export default function MyHeader(props) {
+  const prjctLink = (
+    <Link
+      to={'projects'}
+      spy={true}
+      smooth={true}
+      delay={100}
+      duration={400}
+      onClick={() => {
+        props.setOpen(props.open);
+      }}
+    >
+      Projects {''}
+      <FontAwesomeIcon icon={faAngleDoubleDown} />
+    </Link>
+  );
+
+  const prjctClose = props.open ? (
+    prjctLink
+  ) : (
+    <button
+      style={{ fontFamily: 'Montserrat', fontWeight: 800 }}
+      onClick={() => {
+        props.setOpen(props.open);
+      }}
+    >
+      Projects {''}
+      <FontAwesomeIcon icon={faAngleDoubleUp} />
+    </button>
+  );
+
   return (
     <header>
       <div className="jumbo container">
         <h1>Christian Aquino</h1>
         <h4>Web Design & Front End Development</h4>
-        <Link
-          to={props.open ? 'projects' : ''}
-          spy={true}
-          smooth={true}
-          delay={100}
-          duration={700}
-          onClick={() => {
-            props.setOpen(props.open);
-          }}
-        >
-          Projects
-        </Link>
+        {prjctClose}
       </div>
     </header>
   );
