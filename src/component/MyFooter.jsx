1f2723b6cb8babc8b4c6300a175d747a78c62f19
Christian Aquino <cjfaquino@gmail.com>
2020-10-21 20:51:01 -0700

    update styling

--- src/component/MyFooter.jsx
+++ src/component/MyFooter.jsx
@@ -7,20 +7,23 @@ export default function MyFooter() {
   return (
     <footer id="contact">
       <div className="container">
-        <img src="/imgs/0.jpg" alt="" />
         <div className="info">
-          <span>
-            <FontAwesomeIcon icon={faStreetView} /> Los Angeles, CA
-          </span>
-          <a href="https://www.linkedin.com/in/cjfaquino/">
-            <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
-          </a>
-          <a href="https://github.com/cjfaquino">
-            <FontAwesomeIcon icon={faGithubSquare} /> GitHub
-          </a>
-          <a href="mailto:cjfaquino@outlook.com">
-            <FontAwesomeIcon icon={faEnvelopeSquare} /> Email
-          </a>
+          <div className="footerLinks">
+            <a href="https://www.linkedin.com/in/cjfaquino/">
+              <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+            </a>
+            <a href="https://github.com/cjfaquino">
+              <FontAwesomeIcon icon={faGithubSquare} /> GitHub
+            </a>
+            <a href="mailto:cjfaquino@outlook.com">
+              <FontAwesomeIcon icon={faEnvelopeSquare} /> Email
+            </a>
+          </div>
+          <div>
+            <span className="location">
+              <FontAwesomeIcon icon={faStreetView} /> Los Angeles, CA
+            </span>
+          </div>
         </div>
       </div>
     </footer>
